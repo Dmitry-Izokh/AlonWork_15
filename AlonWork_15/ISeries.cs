d55ec92f03dadc9a847bc9026e8aef6665b16145
Дмитрий Изох <izokh_dv@mail.ru>
2022-11-17 00:08:59 +0300

    Добавьте файлы проекта.

---
+++ AlonWork_15/ISeries.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_15
+{
+    interface ISeries
+    {
+        /* Разработать интерфейс ISeries генерации ряда чисел. Интерфейс содержит следующие элементы:
+         * метод void SetStart(int x) - устанавливает начальное значение
+         * метод int GetNext() - возвращает следующее число ряда
+         * метод void Reset() - выполняет сброс к начальному значению
+         * Разработать классы ArithProgression и GeomProgression, которые реализуют интерфейс ISeries. 
+         * В классах реализовать методы интерфейса в соответствии с логикой арифметической и геометрической
+         * прогрессии соответственно. */
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startValue;
+        int currentValue;
+        int nunberStep;
+
+        public int GetNext()
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public int GetNext2()
+        {
+            currentValue *= step;
+            return currentValue;
+        }
+
+        public void Reset()
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)
+        {
+            startValue=x;
+            currentValue = startValue;
+        }
+
+        public void setStep(int s)
+        {
+            step = s;
+        }
+
+        public void ShowStart()
+        {
+            Console.WriteLine(startValue);
+        }
+
+        public void numberStep(int i)
+        {
+            nunberStep = i;
+        }
+
+        public void NStep()
+        {
+            int n = nunberStep;
+            for(int a=0; a<=n; a++)
+            {
+                Console.Write("Шаг {0} = ", a);
+                Console.WriteLine(GetNext());
+            }            
+        }
+        public void NStep2()
+        {
+            int n = nunberStep;
+            for (int a = 0; a <= n; a++)
+            {
+                Console.Write("Шаг {0} = ", a);
+                Console.WriteLine(GetNext2());
+            }
+        }
+
+
+    }
+}
---
+++ AlonWork_15/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlonWork_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArithProgression arith = new ArithProgression();
+            bool isCorrect = true;
+            
+            do
+            {
+                Console.WriteLine("Какую математическую операцию необходимо произвести?");
+                Console.WriteLine("1 - Арифметическая прогрессия");
+                Console.WriteLine("2 - Геометрическая прогрессия");
+                int n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                switch (n)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите число");
+                            arith.SetStart(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("Введите значение шага арифметической прогрессии");
+                            arith.setStep(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("Введите значение количество шагов арифметической прогрессии");
+                            arith.numberStep(Convert.ToInt32(Console.ReadLine()));
+                            arith.NStep();
+                            Console.WriteLine();
+
+                            Console.WriteLine("Выполнить новый расчет или выключить программу?");
+                            Console.WriteLine("y - Выполнить новый расчет");
+                            Console.WriteLine("Любая другая клавиша - Закрыть программу");
+                            string choise = Console.ReadLine();
+                            
+                            if (choise == "y")
+                            { 
+                                isCorrect = true;
+                                arith.Reset();
+                                Console.Write("Значение сброшено до стартового значения = ");
+                                arith.ShowStart();
+                                Console.WriteLine();
+                            }
+
+                            else
+                            {
+                                isCorrect = false;
+                            }
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Введите число");
+                            arith.SetStart(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("Введите значение шага арифметической прогрессии");
+                            arith.setStep(Convert.ToInt32(Console.ReadLine()));
+                            Console.WriteLine("Введите значение количество шагов арифметической прогрессии");
+                            arith.numberStep(Convert.ToInt32(Console.ReadLine()));
+                            arith.NStep2();
+                            Console.WriteLine();
+
+                            Console.WriteLine("Выполнить новый расчет или выключить программу?");
+                            Console.WriteLine("Y - Выполнить новый расчет");
+                            Console.WriteLine("Любая другая клавиша - Закрыть программу");
+                            string choise = Console.ReadLine();
+
+                            if (choise == "y")
+                            {
+                                isCorrect = true;
+                                arith.Reset();
+                                Console.Write("Значение сброшено до стартового значения = ");
+                                arith.ShowStart();
+                                Console.WriteLine();
+                            }
+
+                            else
+                            {
+                                isCorrect = false;
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Ошибка выбора");
+                            break;
+                        }
+
+                }
+            }
+            while (isCorrect == true);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
